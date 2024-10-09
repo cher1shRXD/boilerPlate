d3aed2e2df2a059acdf1d378abcc26320c945ca8
cher1shRXD <taewoo080401@gmail.com>
2024-10-09 10:08:15 +0900

    feat::automatically add remote to git

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 "use strict";
 
@@ -6,6 +6,7 @@ const { execSync } = require("child_process");
 const path = require("path");
 const chalk = require("chalk");
 const fs = require("fs");
+const readline = require("readline");
 
 if (process.argv.length < 3) {
   console.log(chalk.red("You have to provide a name to your app."));
@@ -20,7 +21,16 @@ const projectPath =
   projectName === "." || projectName === "./"
     ? currentPath
     : path.join(currentPath, projectName);
-const GIT_REPO = "https://github.com/cher1shRXD/cher1sh-react-app";
+
+// 사용자 입력을 받기 위한 readline 인터페이스 설정
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuestion(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
 
 async function main() {
   try {
@@ -35,8 +45,12 @@ async function main() {
       process.exit(1);
     }
 
+    // 사용자에게 Git repository URL 입력받기
+    const gitRepo = await askQuestion("git repository: ");
+    rl.close(); // readline 종료
+
     console.log(chalk.blue("Downloading files..."));
-    execSync(`git clone --depth 1 ${GIT_REPO} ${projectPath}`);
+    execSync(`git clone --depth 1 ${gitRepo} ${projectPath}`);
 
     if (projectName !== "." && projectName !== "./") {
       process.chdir(projectPath);
@@ -47,6 +61,12 @@ async function main() {
 
     console.log(chalk.blue("Removing useless files..."));
     execSync("npx rimraf ./.git");
+
+    console.log(chalk.blue("Initializing new git repository..."));
+    execSync("git init", { stdio: "inherit" });
+
+    console.log(chalk.blue("Adding remote repository..."));
+    execSync(`git remote add origin ${gitRepo}`, { stdio: "inherit" });
 
     console.log(
       chalk.cyan(
